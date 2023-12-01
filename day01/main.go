83ccf5d00235ada6be54fccdbe1ebd0a5675c08b
Leonardo Ferreira <leonardoff.eng@gmail.com>
2023-12-01 21:07:44 +0100

    solve second part day01

--- day01/main.go
+++ day01/main.go
@@ -26,7 +26,36 @@ func getLastDigit(line string) (string, bool) {
 	return "", false
 }
 
-func main() {
+func convertSpelledOutNumbers(input *string) {
+	spelledOutNumbers := [9]string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	for _, number := range spelledOutNumbers {
+		if strings.Contains(*input, number) {
+			*input = strings.ReplaceAll(*input, number, fmt.Sprintf("%s%s%s", number, spelledOutNumbertoNumeral(number), number))
+		}
+	}
+	for _, number := range spelledOutNumbers {
+		if strings.Contains(*input, number) {
+			*input = strings.ReplaceAll(*input, number, "")
+		}
+	}
+}
+
+func spelledOutNumbertoNumeral(number string) string {
+	translation := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+	return translation[number]
+}
+
+func firstPart() {
 	inputFileName := "input.txt"
 	content, err := ioutil.ReadFile(inputFileName)
 	if err != nil {
@@ -45,9 +74,41 @@ func main() {
 		}
 		finalDigit, err := strconv.Atoi(firstDigit + lastDigit)
 		if err != nil {
-			panic("couldn't convert combined digits to int")
+			panic("could not convert combined digits to int")
 		}
 		sum += finalDigit
 	}
-	fmt.Println("Answer: ", sum)
+	fmt.Println("Part 1 Answer: ", sum)
+}
+
+func secondPart() {
+	inputFileName := "input.txt"
+	content, err := ioutil.ReadFile(inputFileName)
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(string(content), "\n")
+	sum := 0
+	for _, line := range lines {
+		convertSpelledOutNumbers((&line))
+		firstDigit, ok := getFirstDigit(line)
+		if !ok {
+			panic("no number found")
+		}
+		lastDigit, ok := getLastDigit(line)
+		if !ok {
+			panic("no number found")
+		}
+		finalDigit, err := strconv.Atoi(firstDigit + lastDigit)
+		if err != nil {
+			panic("could not convert combined digits to int")
+		}
+		sum += finalDigit
+	}
+	fmt.Println("Part 2 Answer: ", sum)
+}
+
+func main() {
+	firstPart()
+	secondPart()
 }
