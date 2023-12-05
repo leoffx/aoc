1341b14976487d75d1e2247e5623629b46482c42
Leonardo Ferreira <leonardoff.eng@gmail.com>
2023-12-05 21:39:45 +0100

    add second part day05

--- day05/main.go
+++ day05/main.go
@@ -92,16 +92,54 @@ func firstPart() {
 	fmt.Println("Part 1 Answer: ", ans)
 }
 
-// func secondPart() {
-// 	inputFileName := "input.txt"
-// 	content, err := os.ReadFile(inputFileName)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	lines := strings.Split(string(content), "\n")
-// }
+func secondPart() {
+	inputFileName := "input.txt"
+	content, err := os.ReadFile(inputFileName)
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(string(content), "\n")
+
+	seedsSplit := strings.Split(lines[0], ": ")
+	if len(seedsSplit) != 2 {
+		panic("malformed seeds line")
+	}
+	seeds, err := stringsToInts(strings.Split(seedsSplit[1], " "))
+	if err != nil {
+		panic(err)
+	}
+	maps, err := parseMaps(&lines)
+	if err != nil {
+		panic(err)
+	}
+	ans := -1
+	for i := 0; i < len(seeds)/2; i += 2 {
+		seedStart := seeds[i]
+		seedLen := seeds[i+1]
+		for i := 0; i < seedLen; i++ {
+			seed := seedStart + i
+			currentKey := seed
+			for _, mapType := range mapOrder {
+				mapRanges := maps[mapType]
+				for _, mapRange := range mapRanges {
+					dstRange := mapRange[0]
+					srcRange := mapRange[1]
+					lenRange := mapRange[2]
+					if currentKey >= srcRange && currentKey < srcRange+lenRange {
+						currentKey = dstRange + (currentKey - srcRange)
+						break
+					}
+				}
+			}
+			if currentKey < ans || ans == -1 {
+				ans = currentKey
+			}
+		}
+	}
+	fmt.Println("Part 2 Answer: ", ans)
+}
 
 func main() {
 	firstPart()
-	// secondPart()
+	secondPart()
 }
