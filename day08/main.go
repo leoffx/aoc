1c2fbf28b5c6296e823340e09ffa5965bb103dd8
Leonardo Ferreira <leonardoff.eng@gmail.com>
2023-12-08 11:33:08 +0100

    add second part day08

--- day08/main.go
+++ day08/main.go
@@ -6,8 +6,20 @@ import (
 	"strings"
 )
 
-func parseNodes(nodes *[]string) (map[string][]string, error) {
-	nodesMap := make(map[string][]string)
+type NodesToDirections = map[string][]string
+
+func getStartingNodes(nodesMap *NodesToDirections) []string {
+	var startingNodes []string
+	for node := range *nodesMap {
+		if strings.HasSuffix(node, "A") {
+			startingNodes = append(startingNodes, node)
+		}
+	}
+	return startingNodes
+}
+
+func parseNodes(nodes *[]string) (NodesToDirections, error) {
+	nodesMap := make(NodesToDirections)
 	for _, node := range *nodes {
 		if node == "" {
 			continue
@@ -59,18 +71,77 @@ outer:
 	}
 	fmt.Println("Part 1 Answer: ", steps)
 }
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
 
-// func secondPart() {
-// 	inputFileName := "input.txt"
-// 	content, err := os.ReadFile(inputFileName)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	lines := strings.Split(string(content), "\n")
-// 	fmt.Println("Part 2 Answer: ", lines)
-// }
+func lcm(a, b int) int {
+	return a / gcd(a, b) * b
+}
+
+func lcmOfArray(arr []int) int {
+	result := arr[0]
+	for i := 1; i < len(arr); i++ {
+		result = lcm(result, arr[i])
+	}
+	return result
+}
+
+func secondPart() {
+	inputFileName := "input.txt"
+	content, err := os.ReadFile(inputFileName)
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(string(content), "\n")
+	directions := lines[0]
+	nodesStr := lines[1:]
+	nodesMap, err := parseNodes(&nodesStr)
+	if err != nil {
+		panic(err)
+	}
+	currNodesKeys := getStartingNodes(&nodesMap)
+	steps := 0
+	canEnd := false
+	hitZIdx := make([]int, len(currNodesKeys))
+outer:
+	for {
+		for _, direction := range directions {
+			canEnd = true
+			for _, i := range hitZIdx {
+				if i == 0 {
+					canEnd = false
+				}
+			}
+			if canEnd {
+				break outer
+			}
+			canEnd = true
+			steps += 1
+			for idx, currNodeKey := range currNodesKeys {
+				if direction == 'R' {
+					currNodesKeys[idx] = nodesMap[currNodeKey][1]
+				} else if direction == 'L' {
+					currNodesKeys[idx] = nodesMap[currNodeKey][0]
+				} else {
+					panic(fmt.Sprintf("unreachable direction %q", direction))
+				}
+				if !strings.HasSuffix(currNodesKeys[idx], "Z") {
+					canEnd = false
+				} else if hitZIdx[idx] == 0 {
+					hitZIdx[idx] = steps
+				}
+			}
+		}
+	}
+	ans := lcmOfArray(hitZIdx)
+	fmt.Println("Part 2 Answer: ", ans)
+}
 
 func main() {
 	firstPart()
-	// secondPart()
+	secondPart()
 }
--- day08/main_test.go
+++ day08/main_test.go
@@ -11,7 +11,7 @@ func TestParseNodes(t *testing.T) {
 		"BBB = (AAA, ZZZ)",
 		"ZZZ = (ZZZ, ZZZ)",
 	}
-	expected := map[string][]string{
+	expected := NodesToDirections{
 		"AAA": {"BBB", "BBB"},
 		"BBB": {"AAA", "ZZZ"},
 		"ZZZ": {"ZZZ", "ZZZ"},
@@ -19,5 +19,23 @@ func TestParseNodes(t *testing.T) {
 	got, err := parseNodes(&input)
 	if !reflect.DeepEqual(got, expected) || err != nil {
 		t.Fatalf("test parse nodes fail. expected=%q got=%q", expected, got)
+	}
+}
+
+func TestGetStartingNodes(t *testing.T) {
+	input := NodesToDirections{
+		"11A": {"11B", "XXX"},
+		"11B": {"XXX", "11Z"},
+		"11Z": {"11B", "XXX"},
+		"22A": {"22B", "XXX"},
+		"22B": {"22C", "22C"},
+		"22C": {"22Z", "22Z"},
+		"22Z": {"22B", "22B"},
+		"XXX": {"XXX", "XXX"},
+	}
+	expected := []string{"11A", "22A"}
+	got := getStartingNodes(&input)
+	if !reflect.DeepEqual(got, expected) {
+		t.Fatalf("test get starting nodes fail. expected=%q got=%q", expected, got)
 	}
 }
